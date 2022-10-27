eb39c700d1d9101a6046a660556441d76e0a1410
DiegoLibonati <diego.libonati1998@gmail.com>
2022-10-27 20:02:25 -0300

    New repository!

---
+++ simple_calculator.py
@@ -0,0 +1,62 @@
+from tkinter import *
+
+class Calculator():
+    def __init__(self, master):
+        master.title("Calculator")
+        master.geometry("357x420+0+0")
+        master.config(bg="gray")
+        master.resizable(False, False)
+
+        self.equation = StringVar()
+        self.equation.set("0")
+        self.entry_value = ""
+        Entry(width= 17,bg="#6C6F7F", font=("Arial Bold", 28), textvariable=self.equation).place(x=0, y=0)
+
+        Button(width=11, height=4, text="(", relief="flat", bg="white", command=lambda:self.show("(")).place(x=0, y=50)
+        Button(width=11, height=4, text=")", relief="flat", bg="white", command=lambda:self.show(")")).place(x=90, y=50)
+        Button(width=11, height=4, text="%", relief="flat", bg="white", command=lambda:self.show("%")).place(x=180, y=50)
+        Button(width=11, height=4, text="1", relief="flat", bg="white", command=lambda:self.show(1)).place(x=0, y=125)
+        Button(width=11, height=4, text="2", relief="flat", bg="white", command=lambda:self.show(2)).place(x=90, y=125)
+        Button(width=11, height=4, text="3", relief="flat", bg="white", command=lambda:self.show(3)).place(x=180, y=125)
+        Button(width=11, height=4, text="4", relief="flat", bg="white", command=lambda:self.show(4)).place(x=0, y=200)
+        Button(width=11, height=4, text="5", relief="flat", bg="white", command=lambda:self.show(5)).place(x=90, y=200)
+        Button(width=11, height=4, text="6", relief="flat", bg="white", command=lambda:self.show(6)).place(x=180, y=200)
+        Button(width=11, height=4, text="7", relief="flat", bg="white", command=lambda:self.show(7)).place(x=0, y=275)
+        Button(width=11, height=4, text="8", relief="flat", bg="white", command=lambda:self.show(8)).place(x=180, y=275)
+        Button(width=11, height=4, text="9", relief="flat", bg="white", command=lambda:self.show(9)).place(x=90, y=275)
+        Button(width=11, height=4, text="0", relief="flat", bg="white", command=lambda:self.show(0)).place(x=90, y=350)
+        Button(width=11, height=4, text=".", relief="flat", bg="white", command=lambda:self.show(".")).place(x=180, y=350)
+        Button(width=11, height=4, text="+", relief="flat", bg="white", command=lambda:self.show("+")).place(x=270, y=275)
+        Button(width=11, height=4, text="-", relief="flat", bg="white", command=lambda:self.show("-")).place(x=270, y=200)
+        Button(width=11, height=4, text="/", relief="flat", bg="white", command=lambda:self.show("/")).place(x=270, y=50)
+        Button(width=11, height=4, text="x", relief="flat", bg="white", command=lambda:self.show("*")).place(x=270, y=125)
+        Button(width=11, height=4, text="=", relief="flat", bg="#5C6672", command=lambda:self.solve()).place(x=270, y=350)
+        Button(width=11, height=4, text="C", relief="flat", bg="#5C6672", command=lambda:self.clear()).place(x=0, y=350)
+
+    def show(self, value):
+
+        if self.entry_value == "ERROR" or self.entry_value == "0":
+            self.entry_value = ""
+            self.equation.set(self.entry_value)
+            self.entry_value += str(value)
+            self.equation.set(self.entry_value)
+        else:
+            self.entry_value += str(value)
+            self.equation.set(self.entry_value)
+
+    def clear(self):
+        self.entry_value = ""
+        self.equation.set(self.entry_value)
+
+    def solve(self):
+        try:
+            result=eval(self.entry_value)
+            self.equation.set(result)
+            self.entry_value = f"{result}"
+        except:
+            self.entry_value = "ERROR"
+            self.equation.set(self.entry_value)
+
+root = Tk()
+calculator = Calculator(root)
+root.mainloop()
